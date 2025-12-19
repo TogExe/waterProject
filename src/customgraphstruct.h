c1611faeb5b3bc77bf394fad74833f043a1490d1
TogExe <66035138+TogExe@users.noreply.github.com>
2025-12-19 15:56:09 +0100

    Update customgraphstruct.h

--- src/customgraphstruct.h
+++ src/customgraphstruct.h
@@ -13,7 +13,8 @@ IndNode *equilibateAVL(IndNode *a);
 IndNode *AddNode(IndNode *f, char id[11], void *element);
 void FreeAVL(IndNode *a);
 void prinfcs(IndNode *ind);
-void printinafilecapa(IndNode * ind, FILE * f);
+void printMaxCapa(IndNode * ind);
+//void printinafilecapa(IndNode * ind, FILE * f);
 void tog_index_add(TogIndex *ind, char id[11], void *element);
 IndNode *tog_index_find_IndNode(TogIndex *ind, char id[11]);
 
