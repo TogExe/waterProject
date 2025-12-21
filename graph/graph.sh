82d3fe691e1074025fdc61779e9fa72d1fe6ddff
Craapul95 <167770049+Craapul95@users.noreply.github.com>
2025-12-21 17:58:48 +0100

    Update graph.sh

--- graph/graph.sh
+++ graph/graph.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-DATA="histo_all.dat"
+DATA="$2"
 
 
 if [["$1" = "max"]] then
-gnuplot -e "datafile='${DATA}'" graphhigh-Vol+Real.gp
+gnuplot -e "datafile='${DATA}'" graphhigh-Max.gp
 
-gnuplot -e "datafile='${DATA}'" graphlow-Vol+Real.gp
+gnuplot -e "datafile='${DATA}'" graphlow-Max.gp
 fi
 
 
-if [["$1" = "vol"]] then
-gnuplot -e "datafile='${DATA}'" graphlow-Vol.gp
+if [["$1" = "src"]] then
+gnuplot -e "datafile='${DATA}'" graphlow-Src.gp
 
-gnuplot -e "datafile='${DATA}'" graphhigh-Vol.gp
+gnuplot -e "datafile='${DATA}'" graphhigh-Src.gp
 fi
 
 if [["$1" = "real"]] then
